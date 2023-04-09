3da831a703e4525c2d31ebfa308257568746e73f
Abhishek6416 <abhishekkr6416@gmail.com>
2023-04-09 16:31:42 +0530

    update and delete route working succesfully

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -31,13 +31,17 @@ router.post("/register", async (req, res) => {
   //LOGIN
 router.post("/login", async (req, res) => {
     try {
+
+        //user===jo hmlog bahar se add kr rhe hai agar same hua toh okk otheriwse error
+
+
       const user = await User.findOne({ email: req.body.email });
       !user && res.status(404).json("user not found");
   
       const validPassword = await bcrypt.compare(req.body.password, user.password)
       !validPassword && res.status(400).json("wrong password")
   
-      res.status(200).json(user)
+      res.status(200).json(user)     //if user enters valid email and password
     } catch (err) {
       res.status(500).json(err)
     }
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,58 @@
 const router=require('express').Router();
+const User=require('../models/User')
+const bcrypt=require('bcrypt')
 
-router.get('/',(req,res)=>{
 
-    res.send("hello from routes")
-})
+
+//update user==will use put request
+
+//normally bohut sarre ids rehta hai abb kaun s id update krna hai woh dekhna parta hai
+//toh hmlog as a user daalte hai id agar woh id databse k ide se match khaata hai then only id update hoga 
+//otherwise it will show error u cant update ur id please enter valid id
+
+router.put("/:id", async (req, res) => {
+    if (req.body.userId === req.params.id || req.body.isAdmin) {
+      if (req.body.password) {
+        try {
+          const salt = await bcrypt.genSalt(10);
+          req.body.password = await bcrypt.hash(req.body.password, salt);
+        } catch (err) {
+          return res.status(500).json(err);
+        }
+      }
+      try {
+        const user = await User.findByIdAndUpdate(req.params.id, {
+          $set: req.body,
+        });
+        res.status(200).json("Account has been updated");
+      } catch (err) {
+        return res.status(500).json(err);
+      }
+    } else {
+      return res.status(403).json("You can update only your account!");
+    }
+  });
+
+  
+//   delete a user
+
+router.delete("/:id", async (req, res) => {
+    if (req.body.userId === req.params.id || req.body.isAdmin) {
+      
+      try {
+         await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("Account has been deleted");
+      } catch (err) {
+        return res.status(500).json(err);
+      }
+    } else {
+      return res.status(403).json("You can delete only your account!");
+    }
+  });
+
+
+
+
 
 
 module.exports=router;
