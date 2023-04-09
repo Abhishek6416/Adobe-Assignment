c05231c76b00830b650cdad3bd298efd13e74c95
Abhishek6416 <abhishekkr6416@gmail.com>
2023-04-10 01:38:18 +0530

    navbar created

---
+++ client/src/pages/home/Home.js
@@ -0,0 +1,12 @@
+import Topbar from "../../components/topbar/Topbar";
+
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+
+        <Topbar/>
+    </div>
+  )
+}
